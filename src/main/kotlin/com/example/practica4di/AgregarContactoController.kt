f663d80f432f84d754b775c5be7d371d811d2488
Borja <brb0004@alu.medac.es>
2023-11-05 19:50:37 +0100

    Commit tarde del 5/11

--- src/main/kotlin/com/example/practica4di/AgregarContactoController.kt
+++ src/main/kotlin/com/example/practica4di/AgregarContactoController.kt
@@ -1,5 +1,24 @@
 package com.example.practica4di
 
+import javafx.event.ActionEvent
+import javafx.fxml.FXML
+import javafx.fxml.FXMLLoader
+import javafx.scene.Node
+import javafx.scene.Parent
+import javafx.scene.Scene
+import javafx.stage.Stage
+import java.io.IOException
+
 class AgregarContactoController {
 
+    @FXML
+    private fun onVolverButtonClick(event: ActionEvent) {
+        try {
+            val node = event.source as Node
+            val stage = node.scene.window as Stage
+            stage.close()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
 }
