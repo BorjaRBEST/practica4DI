1c67490a482b3631222e6ceedce6378339c4ebd2
Borja <brb0004@alu.medac.es>
2023-11-05 12:08:50 +0100

    Commit inicio de proyecto

--- src/main/kotlin/com/example/practica4di/HelloApplication.kt
+++ src/main/kotlin/com/example/practica4di/HelloApplication.kt
@@ -7,9 +7,9 @@ import javafx.stage.Stage
 
 class HelloApplication : Application() {
     override fun start(stage: Stage) {
-        val fxmlLoader = FXMLLoader(HelloApplication::class.java.getResource("hello-view.fxml"))
-        val scene = Scene(fxmlLoader.load(), 320.0, 240.0)
-        stage.title = "Hello!"
+        val fxmlLoader = FXMLLoader(HelloApplication::class.java.getResource("vista-principal.fxml"))
+        val scene = Scene(fxmlLoader.load(), 450.0, 400.0)
+        stage.title = "Práctica 4 Desarrollo de Interfaces"
         stage.scene = scene
         stage.show()
     }
