b9b7d8ce27620620a356fae4095922cf6b03c4fd
Borja <brb0004@alu.medac.es>
2023-11-06 09:08:04 +0100

    Commit mañana del 6/11

--- src/main/kotlin/com/example/practica4di/EliminarContactoController.kt
+++ src/main/kotlin/com/example/practica4di/EliminarContactoController.kt
@@ -1,4 +1,24 @@
 package com.example.practica4di
 
+import javafx.event.ActionEvent
+import javafx.fxml.FXML
+import javafx.scene.Node
+import javafx.stage.Stage
+import java.io.IOException
+
 class EliminarContactoController {
+    // Función que se ejecuta al hacer clic en el botón "Volver"
+    @FXML
+    private fun onVolverButtonClick(event: ActionEvent) {
+        try {
+            // Obtener el nodo fuente (en este caso, el botón)
+            val node = event.source as Node
+
+            // Obtener la ventana actual y cerrarla
+            val stage = node.scene.window as Stage
+            stage.close()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
 }
