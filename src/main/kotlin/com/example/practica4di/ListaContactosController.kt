86ff73558779646888d02764c99692b3cb17aed6
Borja <brb0004@alu.medac.es>
2023-11-06 13:23:58 +0100

    Commit mañana del 6/11

--- src/main/kotlin/com/example/practica4di/ListaContactosController.kt
+++ src/main/kotlin/com/example/practica4di/ListaContactosController.kt
@@ -1,25 +1,49 @@
 package com.example.practica4di
 
+import javafx.collections.FXCollections
 import javafx.event.ActionEvent
 import javafx.fxml.FXML
 import javafx.scene.Node
+import javafx.scene.control.Button
+import javafx.scene.control.ListView
 import javafx.stage.Stage
 import java.io.IOException
 
 class ListaContactosController {
 
-    // Función que se ejecuta al hacer clic en el botón "Volver"
-    @FXML
-    private fun onVolverButtonClick(event: ActionEvent) {
-        try {
-            // Obtener el nodo fuente (en este caso, el botón)
-            val node = event.source as Node
+    private lateinit var gestorDeContactos: GestorDeContactos
 
-            // Obtener la ventana actual y cerrarla
+    @FXML
+    lateinit var listUsuarios: ListView<String>
+
+    @FXML
+    lateinit var buttonVolver: Button
+
+    @FXML
+    lateinit var buttonListarUsuarios: Button
+
+    @FXML
+    fun initialize() {
+        // Inicializar el gestor de contactos
+        this.gestorDeContactos = GestorDeContactos()
+    }
+
+    @FXML
+    fun onVolverButtonClick(event: ActionEvent) {
+        try {
+            val node = event.source as Node
             val stage = node.scene.window as Stage
             stage.close()
         } catch (e: IOException) {
             e.printStackTrace()
         }
+    }
+
+    @FXML
+    fun onListarUsuariosButtonClick(event: ActionEvent) {
+        val listaDeContactos = gestorDeContactos.getListaDeContactos()
+        val nombres = listaDeContactos.map { "${it.nombre} ${it.apellido}" }
+        val items = FXCollections.observableArrayList(nombres)
+        listUsuarios.items = items
     }
 }
